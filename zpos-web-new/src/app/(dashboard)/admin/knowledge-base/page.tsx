a4ce208930c2b12792ee3ceb533f2fc38df54991
Mukopaje <mukopaje@gmail.com>
2026-02-12 17:48:27 +0200

    chore: ignore zpos-web-new folder

--- zpos-web-new/src/app/(dashboard)/admin/knowledge-base/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function KnowledgeBaseIdx() {
-    redirect('/admin/knowledge-base/articles');
-}
--- zpos-web-new/src/app/(dashboard)/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AdminDashboardLayout from "@/components/dashboard/AdminDashboardLayout"
-
-export default function AdminLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    return <AdminDashboardLayout>{children}</AdminDashboardLayout>
-}
